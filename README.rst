44665e7fccea00a813b79fb2c582cd47b6328ebb
Kyle Downey <kyle.downey@gmail.com>
2020-05-17 13:08:21 -0400

    Initial commit of setup files

---
+++ README.rst
@@ -0,0 +1,30 @@
+Pelican FIX Engine
+==================
+
+.. image:: https://dev.azure.com/cloudwall/Pelican/_apis/build/status/cloudwall.pelican?branchName=master
+    :target: https://dev.azure.com/cloudwall/Pelican/_build/latest?definitionId=3
+
+.. image:: https://img.shields.io/pypi/v/pelican-fix.svg
+    :target: https://pypi.org/project/pelican-fix/
+
+.. image:: https://img.shields.io/pypi/l/pelican-fix.svg
+    :target: https://pypi.org/project/pelican-fix/
+
+.. image:: https://img.shields.io/pypi/pyversions/pelican-fix.svg
+    :target: https://pypi.org/project/pelican-fix/
+
+*A framework for building FIX clients and servers in Python*
+
+About Pelican FIX
+-----------------
+
+Pelican FIX is a simple FIX engine with support for arbitrary protocols.
+
+Installation
+------------
+
+Pelican runs on `Python <http://www.python.org/>`_ 3.7 or above. To install Pelican:
+
+.. code:: console
+
+    pip3 install pelican-fix
---
+++ setup.py
@@ -0,0 +1,43 @@
+from glob import glob
+from os.path import splitext, basename
+
+import setuptools
+
+with open('README.rst', 'r') as fh:
+    readme = fh.read()
+
+with open('HISTORY.rst', 'r') as f:
+    history = f.read()
+
+
+requires = [
+    'simplefix'
+]
+
+setuptools.setup(
+    name='pytau',
+    version='0.0.1',
+    description='A Python FIX engine.',
+    long_description=readme + '\n\n' + history,
+    long_description_content_type='text/x-rst',
+    license='MIT',
+    author='Kyle F. Downey',
+    author_email='kyle.downey@gmail.com',
+    url='https://github.com/cloudwall/pelican',
+    packages=setuptools.find_packages('src'),
+    package_dir={'': 'src'},
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    include_package_data=True,
+    python_requires='>=3.7.x',
+    install_requires=requires,
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
+    ),
+)
