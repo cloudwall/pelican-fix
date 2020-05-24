91a5b89e6fd840e7cf2dd93beaf06326c33cf24c
Kyle Downey <kyle.downey@gmail.com>
2020-05-24 13:40:37 -0400

    Add FIXDictionary unit tests for specs from 4.0 to 5.0SP2
    AB#46

--- tests/pythonfix/dictionary/test_parser.py
+++ tests/pythonfix/dictionary/test_parser.py
@@ -3,6 +3,36 @@ from importlib_resources import files, as_file
 from pelicanfix.dictionary.parser import FIXDictionaryParser
 
 
+def test_parse_fix40():
+    source = files('pelicanfix.dictionary').joinpath('FIX40.xml')
+    with as_file(source) as dict_file:
+        parser = FIXDictionaryParser(dict_file)
+        fix_dict = parser.parse()
+        assert fix_dict.major == 4
+        assert fix_dict.minor == 0
+        assert fix_dict.service_pack == 0
+
+        msg = fix_dict.get_message('NewOrderSingle')
+        assert msg.get_msg_type() == 'D'
+        assert msg.get_msg_category() == 'app'
+        assert len(msg.get_elements()) == 36
+
+
+def test_parse_fix41():
+    source = files('pelicanfix.dictionary').joinpath('FIX41.xml')
+    with as_file(source) as dict_file:
+        parser = FIXDictionaryParser(dict_file)
+        fix_dict = parser.parse()
+        assert fix_dict.major == 4
+        assert fix_dict.minor == 1
+        assert fix_dict.service_pack == 0
+
+        msg = fix_dict.get_message('NewOrderSingle')
+        assert msg.get_msg_type() == 'D'
+        assert msg.get_msg_category() == 'app'
+        assert len(msg.get_elements()) == 51
+
+
 def test_parse_fix42():
     source = files('pelicanfix.dictionary').joinpath('FIX42.xml')
     with as_file(source) as dict_file:
@@ -16,6 +46,21 @@ def test_parse_fix42():
         assert msg.get_msg_type() == 'D'
         assert msg.get_msg_category() == 'app'
         assert len(msg.get_elements()) == 71
+
+
+def test_parse_fix43():
+    source = files('pelicanfix.dictionary').joinpath('FIX43.xml')
+    with as_file(source) as dict_file:
+        parser = FIXDictionaryParser(dict_file)
+        fix_dict = parser.parse()
+        assert fix_dict.major == 4
+        assert fix_dict.minor == 3
+        assert fix_dict.service_pack == 0
+
+        msg = fix_dict.get_message('NewOrderSingle')
+        assert msg.get_msg_type() == 'D'
+        assert msg.get_msg_category() == 'app'
+        assert len(msg.get_elements()) == 72
 
 
 def test_parse_fix44():
@@ -47,3 +92,32 @@ def test_parse_fix50():
         assert msg.get_msg_category() == 'app'
         assert len(msg.get_elements()) == 92
 
+
+def test_parse_fix50sp1():
+    source = files('pelicanfix.dictionary').joinpath('FIX50SP1.xml')
+    with as_file(source) as dict_file:
+        parser = FIXDictionaryParser(dict_file)
+        fix_dict = parser.parse()
+        assert fix_dict.major == 5
+        assert fix_dict.minor == 0
+        assert fix_dict.service_pack == 1
+
+        msg = fix_dict.get_message('NewOrderSingle')
+        assert msg.get_msg_type() == 'D'
+        assert msg.get_msg_category() == 'app'
+        assert len(msg.get_elements()) == 92
+
+
+def test_parse_fix50sp2():
+    source = files('pelicanfix.dictionary').joinpath('FIX50SP2.xml')
+    with as_file(source) as dict_file:
+        parser = FIXDictionaryParser(dict_file)
+        fix_dict = parser.parse()
+        assert fix_dict.major == 5
+        assert fix_dict.minor == 0
+        assert fix_dict.service_pack == 2
+
+        msg = fix_dict.get_message('NewOrderSingle')
+        assert msg.get_msg_type() == 'D'
+        assert msg.get_msg_category() == 'app'
+        assert len(msg.get_elements()) == 92
